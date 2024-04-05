a9bd5258daeff89ffbbd6e3759a5836406b23c52
congweiH <congwei_huang@163.com>
2024-04-06 02:39:37 +0800

    实现主要功能

--- star-rating/src/App.jsx
+++ star-rating/src/App.jsx
@@ -1,8 +1,13 @@
 import './App.css'
+import StarRating from './StarRating'
 
 function App() {
   return (
-    <div>hello</div>
+    <div style={{display: "flex", flexDirection: "column", gap: "10px", width: "1000px"}}>
+      <StarRating maxRating={5} />
+      <StarRating maxRating={10} />
+      <StarRating />
+    </div>
   )
 }
 
