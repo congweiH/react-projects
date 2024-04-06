0621d2f26507c7ba14ad400d5d896994fb7d127e
congweiH <congwei_huang@163.com>
2024-04-06 11:16:35 +0800

    添加一些prop，让组件更自定义

--- star-rating/src/App.jsx
+++ star-rating/src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 import StarRating from './StarRating'
 
 function App() {
+  const [rating, setRating] = useState();
+
   return (
     <div style={{display: "flex", flexDirection: "column", gap: "10px", width: "1000px"}}>
-      <StarRating maxRating={5} />
-      <StarRating maxRating={10} />
-      <StarRating />
+      <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} />
+      <StarRating maxRating={10} size={24} color='red' defaultRating={3} onSetRating={setRating} />
     </div>
   )
 }
